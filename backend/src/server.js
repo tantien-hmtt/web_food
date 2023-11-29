6a9ccc9c9389295c41c54938d8bce8bb686e0cd7
occupytheroot <tantien.hmtt@gmail.com>
2023-11-29 20:06:15 +0700

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -38,7 +38,7 @@ app.get('*', (req, res) => {
 });
 
  
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => {
   console.log('listening on port ' + PORT);
 });
