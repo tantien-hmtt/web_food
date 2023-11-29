c93ff46df0fb3e622bb2ba184803cadb5b55253a
occupytheroot <tantien.hmtt@gmail.com>
2023-11-29 21:49:01 +0700

    footer edition

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import classes from "./footer.module.css"
+import { toast } from 'react-toastify';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-solid-svg-icons";
 export default function Footer() {
+  const copyEmailToClipboard = (email) => {
+    const textarea = document.createElement('textarea');
+    textarea.value = email;
+    document.body.appendChild(textarea);
+    textarea.select();
+
+    try {
+      document.execCommand('copy');
+      // Hiển thị toast khi sao chép thành công
+      toast.success('Đã sao chép địa chỉ email vào clipboard!');
+    } catch (err) {
+      console.error('Không thể sao chép vào clipboard:', err);
+      // Hiển thị toast khi có lỗi
+      toast.error('Có lỗi xảy ra khi sao chép địa chỉ email.');
+    } finally {
+      document.body.removeChild(textarea);
+    }
+  };
   return (
     <footer className={classes.footer}>
       <div className={classes.container}> 
@@ -18,10 +37,10 @@ export default function Footer() {
 
         <li className={classes.Contacts}>
           <b className={classes.contact}> Liên hệ với chúng tôi</b>
-          <p> <a href=''>@huynhminhtantien</a></p>
-          <p> <a href=''>@buihoangtrucanh</a></p>
-          <p> <a href=''>@lehoangoanh</a></p>
-          <p> <a href=''>@nguyenngoctramy</a></p>
+          <p> <a onClick={() => copyEmailToClipboard('huynhminhtantien@example.com')}>@huynhminhtantien</a></p>
+            <p> <a onClick={() => copyEmailToClipboard('buihoangtrucanh@example.com')}>@buihoangtrucanh</a></p>
+            <p> <a onClick={() => copyEmailToClipboard('lehoangoanh@example.com')}>@lehoangoanh</a></p>
+            <p> <a onClick={() => copyEmailToClipboard('nguyenngoctramy@example.com')}>@nguyenngoctramy</a></p>
         </li>
         
           <li className={classes.infor}>
